dae9a48a8ed16952c7dfe5387eb73aaf81ff83bb
Edward Harman <jaq+github@ethelred.org>
2024-03-13 08:33:20 -0400

    Checked exception wrapper for Runnable

--- src/main/java/org/ethelred/util/function/CheckedRunnable.java
+++ src/main/java/org/ethelred/util/function/CheckedRunnable.java
@@ -1,4 +1,20 @@
 package org.ethelred.util.function;
 
-public interface CheckedRunnable {
+@FunctionalInterface
+public interface CheckedRunnable<E extends Throwable> {
+  void run() throws E;
+
+  default Runnable asUnchecked() {
+    return () -> {
+      try {
+        run();
+      } catch (Throwable e) {
+        throw new WrappedCheckedException(e);
+      }
+    };
+  }
+
+  static <EE extends Throwable> Runnable unchecked(CheckedRunnable<EE> runnable) {
+    return runnable.asUnchecked();
+  }
 }
