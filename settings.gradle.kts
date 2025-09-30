715df3f1a73b200db186339f8eaf73a346bf5c8b
Edward Harman <jaq+github@ethelred.org>
2025-09-29 21:42:11 -0400

    Remove picocli-examples - there isn't much there.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ plugins {
 	id("io.freefair.settings.plugin-versions").version("8.14.2")
 }
 rootProject.name = "ethelred_util"
-include("common", "picocli", "picocli-examples")
+include("common", "picocli")
